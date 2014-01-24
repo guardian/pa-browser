42d96fa49e7443ace264f830d5b92008ec77628a
Adam Fisher <adam.fisher@guardian.co.uk>
2014-01-24 16:33:57 +0000

    Add team head to head *SNAPSHOT CLIENT*
    First draft of a team head to head builder. This relies on a new PA
    endpoint and currently depends on my local dev snapshot client.

---
+++ app/model/PA.scala
@@ -0,0 +1,26 @@
+package model
+
+object PA {
+  val teams = List(
+    ("1006", "Arsenal"),
+    ("2", "Aston Villa"),
+    ("52", "Cardiff"),
+    ("4", "Chelsea"),
+    ("5", "Crystal Palace"),
+    ("8", "Everton"),
+    ("55", "Fulham"),
+    ("26", "Hull"),
+    ("9", "Liverpool"),
+    ("11", "Man City"),
+    ("12", "Man Utd"),
+    ("31", "Newcastle"),
+    ("14", "Norwich"),
+    ("18", "Southampton"),
+    ("19", "Spurs"),
+    ("38", "Stoke"),
+    ("39", "Sunderland"),
+    ("65", "Swansea"),
+    ("42", "West Brom"),
+    ("43", "West Ham")
+  )
+}
--- build.sbt
+++ build.sbt
@@ -4,11 +4,13 @@ version := "1.0-SNAPSHOT"
 
 resolvers += "Guardian Github Releases" at "http://guardian.github.com/maven/repo-releases"
 
+resolvers += Resolver.file("Local repo", file(System.getProperty("user.home") + "/.ivy2/local"))(Resolver.ivyStylePatterns)
+
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
   cache,
-  "com.gu" %% "pa-client" % "4.0"
+  "com.gu" %% "pa-client" % "4.1-SNAPSHOT"
 )     
 
 play.Project.playScalaSettings
