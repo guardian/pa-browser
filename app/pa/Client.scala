bd714b707aaeb9334d6160595c0dea9e4c46cf6b
Adam Fisher <adam.fisher@guardian.co.uk>
2014-02-10 16:22:10 +0000

    Add some tests to the project

--- app/pa/Client.scala
+++ app/pa/Client.scala
@@ -10,6 +10,7 @@ import scala.io.Source
 import java.io.{FileReader, File}
 import scala.util.{Failure, Success}
 import play.Logger
+import org.joda.time.format.DateTimeFormat
 
 
 trait Client extends PaClient with Http with ExecutionContexts {
@@ -42,20 +43,31 @@ private object TestClient extends Client {
 //  private val basePath = s"${getClass.getClassLoader.getResource("testdata").getFile}/"
 
   override def get(suffix: String)(implicit context: ExecutionContext): Future[String] = {
+
+    val todayString = DateMidnight.now().toString("yyyyMMdd")
     val filename = {
-      val path = suffix.replace("/", "__")
-      path
+      suffix
+        .replace("/", "__")
+        .replace(todayString, "TODAY")
     }
-    try {
-      val file = play.api.Play.current.getFile(s"test/testdata/$filename")
-      val xml = scala.io.Source.fromFile(file, "UTF-8").getLines().mkString
-      Future(xml)(context)
-    } catch {
-      case t: Throwable => {
-        Logger.info(s"Fetching API response for $suffix")
-        val response = Client.get(suffix)(context)
+    val realApiCallPath = {
+      suffix
+        .replace("KEY", Client.apiKey)
+    }
+
+    current.getExistingFile(s"test/testdata/$filename.xml") match {
+      case Some(file) => {
+        val xml = scala.io.Source.fromFile(file, "UTF-8").getLines().mkString
+        Future(xml)(context)
+      }
+      case None => {
+        Logger.warn(s"Missing fixture for API response: $suffix")
+        val response = Client.get(realApiCallPath)(context)
         response.onComplete {
-          case Success(str) => writeToFile(filename, str)
+          case Success(str) => {
+            Logger.info(s"writing response to testdata, $filename.xml, $str")
+            writeToFile(s"${current.path}/test/testdata/$filename.xml", str)
+          }
           case Failure(writeError) => throw writeError
         }(context)
         response
--- test/TestPlayerController.scala
+++ test/TestPlayerController.scala
@@ -1,23 +1,62 @@
+import model.PA
 import org.specs2.matcher.ShouldMatchers
 import org.specs2.mutable.Specification
+import org.specs2.time.NoTimeConversions
+import pa.GetPaClient
+import play.api.mvc.{AnyContentAsFormUrlEncoded, MultipartFormData}
 import play.api.test._
 import play.api.test.Helpers._
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import util.ExecutionContexts
+import xml.Utility.escape
 
-class TestPlayerController extends Specification {
 
-  "test homepage loads correctly" in new WithBrowser {
-    browser.goTo("/")
-    browser.$("h1").getText must equalTo("Football stats, tables, player cards and a whole lot more")
+class TestPlayerController extends Specification with GetPaClient with ExecutionContexts with NoTimeConversions {
+
+  "test player index loads with players as choices" in new WithApplication {
+    val Some(result) = route(FakeRequest(GET, "/player"))
+    status(result) must equalTo(OK)
+    val content = contentAsString(result)
+
+    val expectedPlayers = Await.result (
+      Future.sequence(PA.teams.prem.map { case (teamId, name) =>
+        client.squad(teamId)
+      }),
+      1.seconds
+    ).flatten
+    expectedPlayers.foreach { player =>
+      val name = escape(player.name).replace("'", "&#x27;")
+      content must contain(name)
+    }
   }
 
-  "test player head to head journey" in new WithBrowser(webDriver = FIREFOX) {
-    val playerPage = browser.goTo("/player")
-    Thread.sleep(5000)
-    playerPage
-      .fill("#id-player1").`with`("312242")
-      .fill("#id-player2").`with`("243973")
-    browser.submit(browser.$("form").get(0))
-    browser.title() must equalTo("Player head to head")
-    Thread.sleep(5000)
+  "test redirects player head2head form submission to correct player page" in new WithApplication {
+    val Some(result) = route(FakeRequest(POST, "/player/head2head", FakeHeaders(), AnyContentAsFormUrlEncoded(Map("player1" -> List("123456"), "player2" -> List("654321")))))
+    status(result) must equalTo(SEE_OTHER)
+    redirectLocation(result) must equalTo(Some("/player/head2head/123456/654321"))
+  }
+
+  "test player head to head card renders correctly" in new WithApplication {
+    val Some(result) = route(FakeRequest(GET, "/player/head2head/237670/193388"))
+    status(result) must equalTo(OK)
+    val content = contentAsString(result)
+
+    content must contain("Emmanuel Adebayor")
+    content must contain("Jermain Defoe")
+  }
+
+  "test redirects player card form submission to correct player page" in new WithApplication {
+    val Some(result) = route(FakeRequest(POST, "/player/card", FakeHeaders(), AnyContentAsFormUrlEncoded(Map("player" -> List("123456")))))
+    status(result) must equalTo(SEE_OTHER)
+    redirectLocation(result) must equalTo(Some("/player/card/123456"))
+  }
+
+  "test player head to head card renders correctly" in new WithApplication {
+    val Some(result) = route(FakeRequest(GET, "/player/card/237670"))
+    status(result) must equalTo(OK)
+    val content = contentAsString(result)
+
+    content must contain("Emmanuel Adebayor")
   }
 }
